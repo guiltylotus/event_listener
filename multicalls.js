fb31d884ebeb91a81a82acbd0b8c1c90f65cb3bf
Nguyen Duc Minh <minh02091997@gmail.com>
2024-01-22 11:43:37 +0700

    multicalls limitation

--- multicalls.js
+++ multicalls.js
@@ -6,38 +6,25 @@ const web3 = new Web3('https://bsc.kyberengineering.io');
 
 const multicall = new Multicall({ web3Instance: web3, tryAggregate: true });
 
-const contractCallContext = [
-    {
-        reference: 'UsdcContract',
-        contractAddress: '0x55d398326f99059fF775485246999027B3197955',
-        abi: [
-            {
-                "constant": true,
-                "inputs": [
-                    {
-                        "name": "_owner",
-                        "type": "address"
-                    }
-                ],
-                "name": "balanceOf",
-                "outputs": [
-                    {
-                        "name": "balance",
-                        "type": "uint256"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-        ],
-        calls: [{ reference: 'balanceOfCall', methodName: 'balanceOf', methodParameters: ['0xd958BfE08aF9419C56D07B451F753DCD9b4eDe1A'] }],
-    }
-];
+let contextCalls = {
+    reference: 'UsdcContract',
+    contractAddress: '0x55d398326f99059fF775485246999027B3197955',
+    abi: erc20ABI,
+    calls: [],
+}
 
-multicall.call(contractCallContext)
+for (let i=0; i<2500; i++) {
+    let call = { reference: 'balanceOfCall', methodName: 'balanceOf', methodParameters: ['0xd958BfE08aF9419C56D07B451F753DCD9b4eDe1A'] }
+    contextCalls['calls'].push(call);
+}
+
+console.log('all call', contextCalls['calls'].length);
+
+
+multicall.call(contextCalls)
 .then((results) => {
-    console.log(results['results']['UsdcContract']);
+    console.log(results);
+    console.log(results['results']['UsdcContract']['callsReturnContext'].length);
     console.log(results['results']['UsdcContract']['callsReturnContext'][0]['returnValues']);
 })
 .catch((error) => {
